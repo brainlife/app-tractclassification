54aae4ec8836d1018a78fa7572d9b4956258a199
Soichi Hayashi <hayashis@iu.edu>
2017-09-18 09:43:17 -0400

    Update start.sh
    back porting changes to make this app run on carbonate.

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 
 #clean up previous job (just in case)
 rm -f finished
-qsub -q preempt $SERVICE_DIR/submit.pbs > jobid
+qsub $SERVICE_DIR/submit.pbs > jobid
 #jobid=`qsub -q preempt SERVICE_DIR/submit.pbs`
 #echo $jobid > jobid
 
